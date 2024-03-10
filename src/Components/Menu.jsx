c5807474be0ae6394a84e3adf31a7420805cca14
Dhruv <Dhruvpandey928@gmail.com>
2024-03-10 21:31:29 +0530

    Modified Files

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,31 +1,61 @@
 import React, { useState } from 'react';
-import './Menu.css'; 
-
+import './Menu.css';
 
 const Menu = () => {
-  var formData = new FormData()
   const [csvFile, setCsvFile] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     if (file && file.type === 'text/csv') {
       setCsvFile(file);
+      setErrorMessage('');
     } else {
-      console.log('Invalid file format. Please upload a CSV file.');
+      setErrorMessage('Invalid file format. Please upload a CSV file.');
     }
   };
+  
 
-  async function handlePrediction (e){
-      e.preventDefault();
-      formData.append("file",csvFile)
-      console.log(formData)
-      const response =  await fetch('https://backendserver-pfj1.onrender.com/data',{
-      method: 'POST',
-      body: formData
-      })
-      const data=await response.json()
-      console.log(data)
-  };
+  async function handlePrediction(e) {
+    e.preventDefault();
+
+    if (!csvFile) {
+      setErrorMessage('No CSV file selected.');
+      return;
+    }
+    const formData = new FormData();
+    formData.append('file', csvFile);
+
+    setLoading(true);
+
+    try {
+      const response = await fetch('https://backendserver-pfj1.onrender.com/data', {
+        method: 'POST',
+        body: formData,
+      });
+      if (response.ok) 
+      {
+        //const jsonString = await response.text();
+        //const parsedData = JSON.parse(jsonString);
+        //setPredictionData(parsedData); 
+        //console.log('Prediction data:', parsedData);
+        //console.log('Prediction data:', jsonString);
+      } 
+      else 
+      {
+        setErrorMessage('Prediction request failed.');
+        console.error('Prediction request failed.');
+      }
+    } catch (error) {
+      setErrorMessage(`Error during prediction: ${error.message}`);
+      console.error('Error during prediction:', error);
+    }
+    finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <div className="menu">
@@ -38,8 +68,10 @@ const Menu = () => {
           accept=".csv"
           onChange={handleFileChange}
         />
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
       </div>
-      <button onClick={handlePrediction}>Get my Sales Prediction</button>
+      <button onClick={handlePrediction} disabled={loading}>Get my Sales Prediction</button>
+      {loading && <p>Loading...</p>}
     </div>
   );
 };
