076dbefd79710890421fb4c07b62ebc35004233f
Bhavya Patel <100468926+BhavyaMPatel@users.noreply.github.com>
2024-03-10 23:09:00 +0530

    Revert "Modified"

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -15,37 +15,47 @@ const Menu = () => {
     } else {
       setErrorMessage('Invalid file format. Please upload a CSV file.');
     }
-  };  
+  };
+  
 
-  //check onlu
-  async function handlePrediction (e){
-      e.preventDefault();
-      formData.append("file",csvFile)
-      console.log(formData)
-      const response =  await fetch('http://127.0.0.1:8000/data',{
-      method: 'POST',
-      body: formData
-      })
-      const data=await response.json()
-      let values=data.data;
-      values = JSON.parse(values);
-      //Check
-      console.log(values)
-      for (const productName in values) {
-        if (values.hasOwnProperty(productName)) {
-            const product = values[productName];
-            console.log(productName);
-            console.log("   Old values:");
-            product.OldValue.forEach(value => console.log(`      ${value}`));
-            console.log("   New values (except null):");
-            product.NewValue.forEach(value => {
-                if (value !== "null") {
-                    console.log(`      ${value}`);
-                }
-            });
-        }
+  async function handlePrediction(e) {
+    e.preventDefault();
+
+    if (!csvFile) {
+      setErrorMessage('No CSV file selected.');
+      return;
     }
-    };
+    const formData = new FormData();
+    formData.append('file', csvFile);
+
+    setLoading(true);
+
+    try {
+      const response = await fetch('https://backendserver-pfj1.onrender.com/data', {
+        method: 'POST',
+        body: formData,
+      });
+      if (response.ok) 
+      {
+        //const jsonString = await response.text();
+        //const parsedData = JSON.parse(jsonString);
+        //setPredictionData(parsedData); 
+        //console.log('Prediction data:', parsedData);
+        //console.log('Prediction data:', jsonString);
+      } 
+      else 
+      {
+        setErrorMessage('Prediction request failed.');
+        console.error('Prediction request failed.');
+      }
+    } catch (error) {
+      setErrorMessage(`Error during prediction: ${error.message}`);
+      console.error('Error during prediction:', error);
+    }
+    finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <div className="menu">
